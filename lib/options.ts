e27072c936dbc83d16a3737ede3c634e8a09106e
Jo Surzn <joanna.surzyn@fullstax.de>
2021-07-12 11:33:49 +0200

    feat: add unmodified enumStyle option

--- lib/options.ts
+++ lib/options.ts
@@ -69,12 +69,13 @@ export interface Options {
    * Determines how root enums will be generated. Possible values are:
    *
    * - `alias`: just generate a type alias with the possible values;
-   * - `upper` for an enum with UPPER_CASE names, and;
-   * - `pascal` for enum PascalCase names.
+   * - `upper` for an enum with UPPER_CASE names;
+   * - `pascal` for enum PascalCase names;
+   * - `unmodified` to use the value as the name after basic character parsing;
    *
    * Defaults to 'pascal'.
    */
-  enumStyle?: 'alias' | 'upper' | 'pascal';
+  enumStyle?: 'alias' | 'upper' | 'pascal' | 'unmodified';
 
   /**
    * Determines how to normalize line endings. Possible values are:
